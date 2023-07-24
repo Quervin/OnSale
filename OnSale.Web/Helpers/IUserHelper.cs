0f28afcf08891d76d51e0e932895a1c677f492df
querv <quervinhidalgo@gmail.com>
2023-07-24 12:24:14 -0600

    final version

--- OnSale.Web/Helpers/IUserHelper.cs
+++ OnSale.Web/Helpers/IUserHelper.cs
@@ -13,6 +13,8 @@ namespace OnSale.Web.Helpers
     {
         Task<User> GetUserAsync(string email);
 
+        Task<User> GetUserAsync(Guid userId);
+
         Task<IdentityResult> AddUserAsync(User user, string password);
 
         Task CheckRoleAsync(string roleName);
@@ -28,6 +30,19 @@ namespace OnSale.Web.Helpers
         Task<SignInResult> ValidatePasswordAsync(User user, string password);
 
         Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, string ImageUrl, UserType userType);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
 
     }
 
